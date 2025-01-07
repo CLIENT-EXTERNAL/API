64cf4082a18d362ec430079927d6fdace41e131d
rubensalban <rubensalban@gmail.com>
2025-01-07 18:31:37 +0100

    PM2 setup

---
+++ ecosystem.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+    apps : [{
+      name: 'MOON',
+      script: 'app.js',
+      instances: 1,
+      watch: '.',
+      ignore_watch: ['node_modules', 'public'],
+    }],
+    deploy : {
+      test : {
+        user : 'root',
+        host : '180.149.196.70',
+        ref  : 'origin/main',
+        repo : 'https://github.com/NYOTA-PROJECTS/LAPOINTE-API.git',
+        path : '/root/API/MOON',
+        'pre-deploy-local': '',
+        'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env test',
+        'pre-setup': ''
+      },
+      development : {
+        user : 'root',
+        host : '180.149.196.70',
+        ref  : 'origin/main',
+        repo : 'https://github.com/NYOTA-PROJECTS/LAPOINTE-API.git',
+        path : '/root/API/MOON',
+        'pre-deploy-local': '',
+        'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env development',
+        'pre-setup': ''
+      },
+      production : {
+        user : 'root',
+        host : '180.149.196.70',
+        ref  : 'origin/main',
+        repo : 'https://github.com/NYOTA-PROJECTS/LAPOINTE-API.git',
+        path : '/root/API/MOON',
+        'pre-deploy-local': '',
+        'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+        'pre-setup': ''
+      }
+    }
+  };
+    
